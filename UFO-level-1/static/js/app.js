3d28ad5f824408b2ee63f914b54728e71e099e92
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-03-13 22:21:38 -0500

    work with geronimo

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,29 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select('tbody');
 
-// YOUR CODE HERE!
+renderTable(tableData);
+d3.selectAll('#filter-btn').on('click', handleClick);
+
+// ==================================================
+function renderTable(data) {
+    tbody.html('');
+
+    data.forEach(dataRow => {
+        var row = tbody.append('tr');
+
+        Object.values(dataRow).forEach(val => {
+            var cell = row.append('td');
+            cell.text(val);
+        });
+    });
+};
+
+function handleClick() {
+    var filteredData = tableData;
+    var date = d3.select('#datetime').property('value');
+    if (date) { filteredData = filteredData.filter(row => row.date === date) };
+    d3.select('#datetime').property('value',"");
+    renderTable(filteredData);
+}
+
